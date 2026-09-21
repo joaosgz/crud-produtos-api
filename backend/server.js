36c1dd95aa5716530b88f1fafa12d1953ed2307b
Joao Gomes <jotapsgz@gmail.com>
2026-09-21 19:23:39 -0300

    Prepara API para hospedagem

--- backend/server.js
+++ backend/server.js
@@ -112,8 +112,8 @@ app.delete("/produtos/:id", (req, res) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
